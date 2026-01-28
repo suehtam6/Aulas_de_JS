9583dafd680a8d5157615bd20e63f59d50d527b1
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-01-28 15:44:49 -0300

    Começando a aprender JavaScript

---
+++ Aula_01/app.js
@@ -0,0 +1,13 @@
+// Começando a primeira aula de JS
+
+// Permite exibir um conteúdo no terminal  
+console.log("Testando JS")
+
+// Criando uma variavel
+var nome = "Matheus";
+
+// Concatenação de dados(Texto e variavel)
+console.log("O nome do usuário é: " + nome)
+
+// Outra forma de fazer a concatenação
+console.log(`O nome do usuário é: ${nome}`)
