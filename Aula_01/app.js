37dbe1eedf9a778f6a11fc25df3ed9264f7a8498
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-01-28 16:55:07 -0300

    Finalizando a primeira aula do dia 28/01/2026

--- Aula_01/app.js
+++ Aula_01/app.js
@@ -1,17 +1,21 @@
 // Começando a primeira aula de JS
 
-// Permite exibir um conteúdo no terminal  
+/* 
+Permite exibir um conteúdo no terminal  
 console.log("Testando JS")
 
-// Criando uma variavel
+Criando uma variavel
 var nome = "Matheus"
 
-// Concatenação de dados(Texto e variavel)
+Concatenação de dados(Texto e variavel)
 console.log("O nome do usuário é: " + nome)
 
-// Outra forma de fazer a concatenação
+Outra forma de fazer a concatenação
 console.log(`O nome do usuário é: ${nome}`)
 
+*/
+
+const console = require("console")
 // Importando a biblioteca readline
 // readline -> serve para permitir a entrada de dados via terminal
 var readline = require("readline")
@@ -29,8 +33,18 @@ var entradaDeDados = readline.createInterface({
     A função .question serve para deixar uma pergunta no terminal e esperando uma resposta.
     Logo a function(nomeUsuario) está sendo utilizada para guardar o nome que for digitado e ser utilizado a algum momento.
 */
-
+// Permite a entrada de dados do nome do usuário
 entradaDeDados.question("Digite o seu nome: ", function(nomeUsuario){
     console.log("O nome digitado foi: " + nomeUsuario)
+
+// Permite a entrada de dados do email do usuário
+    entradaDeDados.question("Digite o seu email: ", function(emailUsuario){
+        console.log(`O email do usuário ${nomeUsuario} é ${emailUsuario}`)
+    })
+
+// Permite a entrada de dados do número do usuário
+    entradaDeDados.question("Digite o seu número: ", function(numeroUsuario){
+        console.log("O número do usuário:" + numeroUsuario)
+    })
 
 })
