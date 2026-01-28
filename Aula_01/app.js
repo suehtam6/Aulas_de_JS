8feee4933a6df29a5a1dd168a742ba1135ddd627
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-01-28 16:33:46 -0300

    Mais alguns passos do JavaScript

--- Aula_01/app.js
+++ Aula_01/app.js
@@ -4,10 +4,33 @@
 console.log("Testando JS")
 
 // Criando uma variavel
-var nome = "Matheus";
+var nome = "Matheus"
 
 // Concatenação de dados(Texto e variavel)
 console.log("O nome do usuário é: " + nome)
 
 // Outra forma de fazer a concatenação
 console.log(`O nome do usuário é: ${nome}`)
+
+// Importando a biblioteca readline
+// readline -> serve para permitir a entrada de dados via terminal
+var readline = require("readline")
+
+//Cria um objeto especializado na entrada de dados pelo terminal
+var entradaDeDados = readline.createInterface({
+
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+/* 
+    Fazendo um callback(chamada de retorno).
+    A função .question serve para deixar uma pergunta no terminal e esperando uma resposta.
+    Logo a function(nomeUsuario) está sendo utilizada para guardar o nome que for digitado e ser utilizado a algum momento.
+*/
+
+entradaDeDados.question("Digite o seu nome: ", function(nomeUsuario){
+    console.log("O nome digitado foi: " + nomeUsuario)
+
+})
