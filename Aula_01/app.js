489864be5ee6d39c7914db84b78738ce161ae30b
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-01-29 16:33:31 -0300

    Fazendo uma validação de dados

--- Aula_01/app.js
+++ Aula_01/app.js
@@ -39,13 +39,17 @@ entradaDeDados.question("Digite o seu nome: ", function(nomeUsuario){
 
 // Permite a entrada de dados do email do usuário
     entradaDeDados.question("Digite o seu email: ", function(emailUsuario){
-        console.log(`O email do usuário ${nomeUsuario} é ${emailUsuario}`)
+        console.log(`O email do usuário ${nomeUsuario} é ${emailUsuario}`)    
+        
+            // Permite a entrada de dados do número do usuário
+            entradaDeDados.question("Digite o seu número: ", function(numeroUsuario){
+                console.log("O número do " + nomeUsuario + " é " + numeroUsuario)
     })
 
-// Permite a entrada de dados do número do usuário
-    entradaDeDados.question("Digite o seu número: ", function(numeroUsuario){
-        console.log("O número do " + nomeUsuario + " é " + numeroUsuario)
+
     })
+
+
 
 })
 
--- Aula_02/app.js
+++ Aula_02/app.js
@@ -3,7 +3,8 @@
  * Autor: Matheus Lucas
  * Data: 29/01/2026
  * Versão: 1.0
- * *********************************************************************************** */
+ *************************************************************************************
+ */
 
 /* 
     Tipos de criação de variaveis
@@ -17,10 +18,109 @@
             que seja utilizada apenas em escopo global.
 
             Escopo global -> Serve para quando for criado uma variavel
-                            no começo do projeto e seja utilizada até o final do projeto 
+                            no começo do projeto e ser utilizada em qualquer local do projeto.
 
-    let -> 
+            Escopo local -> Serve para quando for criado uma variavel
+                            dentro de uma function e ser utilizada somente ali dentro.
 
-    const ->
+    let -> Permite criar um espaço em memória do tipo variavel.
+            Essa forma de criação é realizada somente no escopo local,
+            ou seja, dentro de bloco de programação { }.
+            Esse tipo de variavel deixa de existir ao término do bloco.
+
+    const -> Permite criar um espaço em memória do tipo constante,
+             ou seja, esse conteúdo não poderá sofrer mudanças durante o projeto.
+             Pode ser criada em escopo global e local
+
+             Dica: Se possível você pode criar está const escrita em MAIÚSCULA
+             para facilitar a sua utilização.
+
+    Operadores de comparação
+
+    ==  -> Permite a comparação de dois conteúdos.
+
+    !=  -> Permite comparar a diferença de dois conteúdos.
+
+    <=  -> Permite validar se o valor é menor ou igual.
+
+    >=  -> Permite validar se o valor é maior ou igual.
+
+    <   -> Permite validar o valor menor.
+
+    >   -> Permite validar o valor maior.
+
+    === -> Os dois primeiros "==" comparam  a igualdade dos conteúdo
+           e o ultimo "=" compara a igualdade dos tipos de dados.
+
+    !== -> Os dois primeiros sinais "!=" comparar se o conteúdo é diferente
+           e o ultimo "=" compara a igualdade dos tipos de dados.
+
+    =!= -> Permite comprar a igualdade de conteúdos e a diferença de tipagem de dados.
+             
+    Tipos de operadores lógicos
+        E -> AND -> &&
+        OU -> OR -> ||
+        NAO -> NOT -> !
 
 */
+
+// Import da biblioteca readline
+const readline = require("readline")
+
+// Criação do objeto para captar as entradas de dados
+const entradaDeDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+// Entrada de dados do nome
+entradaDeDados.question("Digite o nome do aluno: ", function (nome) {
+    // Recebe o nome do aluno
+    let nomeAluno = nome
+
+    // Entrada de dados da nota 1
+    entradaDeDados.question("Digite a nota 1: ", function(valor1) {
+        let nota1 = valor1
+
+        // Entrada de dados da nota 2
+        entradaDeDados.question("Digite a nota 2: ", function(valor2) {
+            let nota2 = valor2
+
+            // Entrada de dados da nota 3
+            entradaDeDados.question("Digite a nota 3: ", function(valor3) {
+                let nota3 = valor3
+
+                // Entrada de dados da nota 4
+                entradaDeDados.question("Digite a nota4: ", function(valor4) {
+                    let nota4 = valor4
+
+                    // Validação de entrada vazia utilizando o OU -> OR -> ||   \\
+                    if(nomeAluno == "" || nota1 == "" || nota2 == "" || nota3 == "" || nota4 == ""){
+                        console.log("ERRO: Existem campos obrigatorios que não foram preenchidos !!!")
+
+                    // Validação caso o número seja maior que 100 
+                    }else if(nota1 > 100 || nota2 > 100 || nota3 > 100 || nota4 > 100){
+                        console.log("ERRO: Número maior que 100")
+
+                    // Validação caso o número seja menor que 0
+                    }else if(nota1 < 0 || nota2 < 0 || nota3 < 0 || nota4 < 0){
+                        console.log("ERRO: número menor que 0")
+                    }else{
+                        console.log("calcular")
+                    }
+
+                    /*
+                    // Validação de entrada vazia utilizando o E -> AND -> &&   \\
+                    if(nomeAluno != "" && nota1 != "" && nota2 != "" && nota3 != "" && nota4 != ""){
+                        console.log("Calcular")
+                    }else{
+                        console.log("Erro")
+                    }
+                    
+                    */
+                })
+            })
+        })
+    })
+})
+
