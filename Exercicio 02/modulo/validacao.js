2bcce315ef073bb37403238546c1a818f4780356
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-02-13 16:37:28 -0300

    Trabalho finalizado com sucesso

--- Exercicio 02/modulo/validacao.js
+++ Exercicio 02/modulo/validacao.js
@@ -1,11 +1,11 @@
 function validarDados(operacao, valor1, valor2){
     let nomeOperacao = operacao
-    let v1 = valor1
-    let v2 = valor2
+    let v1 = valor1.replace(/,/g, ".", /([.,])\1/)
+    let v2 = valor2.replace(/,/g, ".", /([.,])\1/)
 
-    if(nomeOperacao == "" ||isNaN(nomeOperacao) == false|| valor1 == "" || valor2 == ""){
+    if(nomeOperacao == "" ||isNaN(nomeOperacao) == false|| v1 == "" || v2 == ""){
         return false
-    }else if(isNaN(valor1) || isNaN(valor2)){
+    }else if(isNaN(v1)|| isNaN(v2)){
         return false
     }
     return true
