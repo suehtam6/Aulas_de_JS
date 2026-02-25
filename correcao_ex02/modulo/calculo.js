48be7cc65e3646746fcc89a8804385228a5cd0ed
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-02-25 13:46:36 -0300

    correção exercicio 02 realizada

--- correcao_ex02/modulo/calculo.js
+++ correcao_ex02/modulo/calculo.js
@@ -13,47 +13,56 @@
 // toLowerCase() -> Deixa todas as letras minusculas
 
 const calcular = function (numero1, numero2, operador) {
-    let valor1 = Number(numero1)
-    let valor2 = Number(numero2)
+    let valor1             = Number(numero1)
+    let valor2             = Number(numero2)
     let operadorMatematico = String(operador).toUpperCase()
 
     let resultado = false
-    // if(operadorMatematico == 'SOMAR')
-    //     resultado = valor1 + valor2
-    // else if(operadorMatematico == 'SUBTRAIR')
-    //     resultado = valor1 - valor2
-    // else if(operadorMatematico == 'MULTIPLICAR')
-    //     resultado = valor1 * valor2
-    // else if(operadorMatematico == 'DIVIDIR')
-    //     resultado = valor1 / valor2
-
-
+    
     switch (operadorMatematico) {
         case 'SOMAR':
-            resultado = valor1 + valor2
+            resultado = somar(valor1, valor2)
             break;
         case 'SUBTRAIR':
-            resultado = valor1 - valor2
+            resultado = subtrair(valor1, valor2)
             break;
 
         case 'MULTIPLICAR':
-            resultado = valor1 * valor2
+            resultado = multiplicar(valor1, valor2)
             break;
 
         case 'DIVIDIR':
-            resultado = valor1 / valor2
+            resultado = dividir(valor1, valor2)
             break;
     }
     return resultado
 
-
 }
 
+// função baseada em formato de seta(ARROW FUNCTION)
+// Ela deve ser feita nessa forma caso sua linha de codigo tenha mais de uma linha
+const somar = (numero1, numero2) => {
+    let valor1 = Number(numero1)
+    let valor2 = Number(numero2)
+
+    let resultado = valor1 + valor2
+
+    return resultado
+}
+
+// Outra forma de utilizar a função seta, ela funciona somente se for em uma linha de codigo
+// const somar = (numero1, numero2) => Number(numero1) + Number(numero2)
+const subtrair = (numero1, numero2) => Number(numero1) - Number(numero2)
+const multiplicar = (numero1, numero2) => Number(numero1) * Number(numero2)
+const dividir = (numero1, numero2) => Number(numero1) / Number(numero2)
+
+
 // chamando a função para testar
-let result = calcular(23, 666, 'somar')
+let result = calcular(23.5, 60, 'multiplicar')
 console.log(result)
 // if(result){
 //     console.log(result)
 // }else{
 //     console.log('ERRO')
 // }
+
