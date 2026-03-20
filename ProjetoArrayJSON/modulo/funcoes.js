98a6790b2ccce0f9b1fbb54af0c9b4894c25f38d
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-03-20 15:11:51 -0300

    Percorrendo os dados de um estado utilizando a UF

--- ProjetoArrayJSON/modulo/funcoes.js
+++ ProjetoArrayJSON/modulo/funcoes.js
@@ -2,7 +2,7 @@
  * Objetivo: Manipular dados dos Estados do Brasil
  * Autor: Matheus Lucas
  * Data: 18/03/2026
- * Versão: 1.0
+ * Versão: 2.0
  **************************************************************************************/
 
 const estados = require('./estados_cidades')
@@ -23,7 +23,45 @@ function getListaDeEstados() {
         estadosSiglas = { uf, 'quantidade': uf.length }
         return estadosSiglas
 
+}
+
+// Verificando os dados do estado de acordo com a UF
+function getDadosEstado(siglaUF) {
+
+        let buscarSigla = siglaUF
+        let dadosEstados = {}
+        let uf
+        let descricao
+        let capital
+        let regiao
+
+        estadosUF.forEach(function (itemUF) {
+                if (!buscarSigla) {
+                        return false
+                }
+                if (String(buscarSigla).toUpperCase() == itemUF.sigla) {
+                        uf = itemUF.sigla
+                        descricao = itemUF.nome
+                        capital = itemUF.capital
+                        regiao = itemUF.regiao
+                }
+
+        })
+
+        dadosEstados = {
+                uf,
+                descricao,
+                capital,
+                regiao
+        }
+        console.log(dadosEstados)
+
+
+
 
 }
 
-getListaDeEstados()
+getDadosEstado('ba')
+
+
+//console.log(getListaDeEstados())
