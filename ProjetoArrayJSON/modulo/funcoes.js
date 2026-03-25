4ae48134e74062fd1c114bb5c7dc45985d3a6f12
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-03-25 15:07:26 -0300

    [FEAT] Função getCapitalPais criada

--- ProjetoArrayJSON/modulo/funcoes.js
+++ ProjetoArrayJSON/modulo/funcoes.js
@@ -10,35 +10,23 @@ const estadosUF = estados.listaDeEstados.estados
 
 // Fazendo uma função para pegar as siglas dos estados(UF)
 function getListaDeEstados() {
-        let estadosSiglas = {}
         let uf = []
 
         estadosUF.forEach(function (itemUF) {
                 uf.push(itemUF.sigla)
         })
 
-        if (estadosSiglas == null) {
-                return false
-        }
         estadosSiglas = { uf, 'quantidade': uf.length }
         return estadosSiglas
 
 }
 
-
-
 // Verificando os dados do estado de acordo com a UF
 function getDadosEstado(siglaUF) {
         let buscarSigla = String(siglaUF).toUpperCase()
         let dadosEstados = {}
-        let uf
-        let descricao
-        let capital
-        let regiao
         estadosUF.forEach(function (itemUF) {
-                if (!buscarSigla) {
-                        return false
-                }
+                
                 if (buscarSigla == itemUF.sigla) {
                         uf = itemUF.sigla
                         descricao = itemUF.nome
@@ -55,13 +43,11 @@ function getDadosEstado(siglaUF) {
         return dadosEstados
 }
 
-
-
 // Verificando os dados da capital de acordo com a UF
 function getCapitalEstado(siglaUF) {
         let buscarSigla = String(siglaUF).toUpperCase()
         let dados = [getDadosEstado(buscarSigla)]
-        let dadosCapital = {}
+
         dados.forEach(function (itensDados) {
                 uf = itensDados.uf
                 descricao = itensDados.descricao
@@ -71,19 +57,14 @@ function getCapitalEstado(siglaUF) {
         return dadosCapital
 }
 
-
+// verificando os dados de acordo com a região
 function getEstadosRegiao(buscarRegiao) {
-        let estadoRegiao = {}
         let regiaoMod = String(buscarRegiao).toUpperCase()
-        let regiao
-        let uf
-        let descricao
         let estados = []
-        let jonson = {}
-        
-        estadosUF.forEach(function(itemRegiao){
-                
-                if(regiaoMod === String(itemRegiao.regiao).toUpperCase()){
+
+        estadosUF.forEach(function (itemRegiao) {
+
+                if (regiaoMod === String(itemRegiao.regiao).toUpperCase()) {
                         uf = itemRegiao.sigla
                         descricao = itemRegiao.nome
                         regiao = itemRegiao.regiao
@@ -91,21 +72,44 @@ function getEstadosRegiao(buscarRegiao) {
                         jonson = {
                                 uf, descricao
                         }
-                        
+
                         estados.push(jonson)
                 }
 
-                
         })
-        
+
         estadoRegiao = {
                 regiao,
                 estados
         }
-       
+
         return estadoRegiao
 }
-// console.log(getCapitalEstado('Sp'))
+
+function getCapitalPais() {
+
+        let capitalPais = {capitais : []}
+
+        estadosUF.forEach(function(itemCapital){
+                if(itemCapital.capital_pais){
+                        capitalPais.capitais.push({
+                                capital_atual : itemCapital.capital_pais.capital,
+                                uf : itemCapital.sigla,
+                                descricao : itemCapital.nome,
+                                capital : itemCapital.capital,
+                                regiao : itemCapital.regiao,
+                                capital_pais_ano_inicio : itemCapital.capital_pais.ano_inicio,
+                                capital_pais_ano_fim : itemCapital.capital_pais.ano_fim
+                        })
+                }
+        })
+        return capitalPais
+       
+}
+
+
+//getCapitalEstado('Sp')
 //getDadosEstado('ba')
 //console.log(getListaDeEstados())
-console.log(getEstadosRegiao('sul'))
+//console.log(getEstadosRegiao('sudeste'))
+//console.log(getCapitalPais())
