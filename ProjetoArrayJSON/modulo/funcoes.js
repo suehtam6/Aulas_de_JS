7e3d05329f8905809b42e6537c010e467126ebd3
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-03-25 16:34:29 -0300

    [feat] Fazendo validações para retornar false

--- ProjetoArrayJSON/modulo/funcoes.js
+++ ProjetoArrayJSON/modulo/funcoes.js
@@ -25,13 +25,19 @@ function getListaDeEstados() {
 function getDadosEstado(siglaUF) {
         let buscarSigla = String(siglaUF).toUpperCase()
         let dadosEstados = {}
+        let uf
+        let descricao
+        let capital
+        let regiao
         estadosUF.forEach(function (itemUF) {
-                
+
                 if (buscarSigla == itemUF.sigla) {
                         uf = itemUF.sigla
                         descricao = itemUF.nome
                         capital = itemUF.capital
                         regiao = itemUF.regiao
+                } else {
+                        return false
                 }
         })
         dadosEstados = {
@@ -49,11 +55,18 @@ function getCapitalEstado(siglaUF) {
         let dados = [getDadosEstado(buscarSigla)]
 
         dados.forEach(function (itensDados) {
-                uf = itensDados.uf
-                descricao = itensDados.descricao
-                capital = itensDados.capital
+                if (String(dados).toUpperCase() === String(itensDados).toUpperCase()) {
+                        dadosCapital = {
+                                uf: itensDados.uf,
+                                descricao: itensDados.descricao,
+                                capital: itensDados.capital
+                        }
+                } else {
+                        return false
+                }
+
         })
-        dadosCapital = { uf, descricao, capital }
+
         return dadosCapital
 }
 
@@ -62,87 +75,111 @@ function getEstadosRegiao(buscarRegiao) {
         let regiaoMod = String(buscarRegiao).toUpperCase()
         let estados = []
 
-        estadosUF.forEach(function (itemRegiao) {
 
-                if (regiaoMod === String(itemRegiao.regiao).toUpperCase()) {
+
+        estadosUF.forEach(function (itemRegiao) {
+                if (!isNaN(regiaoMod) || regiaoMod == '') {
+                        return false
+                }
+                if(regiaoMod === String(itemRegiao.regiao).toUpperCase()) {
                         uf = itemRegiao.sigla
                         descricao = itemRegiao.nome
-                        regiao = itemRegiao.regiao
 
                         jonson = {
                                 uf, descricao
                         }
 
                         estados.push(jonson)
+
+                        estadoRegiao = {
+                                regiao: itemRegiao.regiao,
+                                estados
+                        }
+                        return false
                 }
 
         })
 
-        estadoRegiao = {
-                regiao,
-                estados
+        if(estadoRegiao == null || regiaoMod == '' || regiaoMod == null || !isNaN(regiaoMod) ){
+                return false
+        }else{
+                return estadoRegiao
         }
 
-        return estadoRegiao
+        
+
 }
 
 // FUNÇÃO PARA SABER QUAIS SÃO TODAS AS CAPITAIS QUE O BRASIL JA TEVE
 function getCapitalPais() {
 
-        let capitalPais = {capitais : []}
+        let capitalPais = { capitais: [] }
 
-        estadosUF.forEach(function(itemCapital){
-                if(itemCapital.capital_pais){
+        estadosUF.forEach(function (itemCapital) {
+                if (itemCapital.capital_pais) {
 
                         capitalPais.capitais.push({
 
-                                capital_atual : itemCapital.capital_pais.capital,
-                                uf : itemCapital.sigla,
-                                descricao : itemCapital.nome,
-                                capital : itemCapital.capital,
-                                regiao : itemCapital.regiao,
-                                capital_pais_ano_inicio : itemCapital.capital_pais.ano_inicio,
-                                capital_pais_ano_fim : itemCapital.capital_pais.ano_fim
+                                capital_atual: itemCapital.capital_pais.capital,
+                                uf: itemCapital.sigla,
+                                descricao: itemCapital.nome,
+                                capital: itemCapital.capital,
+                                regiao: itemCapital.regiao,
+                                capital_pais_ano_inicio: itemCapital.capital_pais.ano_inicio,
+                                capital_pais_ano_fim: itemCapital.capital_pais.ano_fim
 
                         })
                 }
         })
-        return capitalPais
-       
+        if(capitalPais == null){
+                return false
+        }else{
+                return capitalPais
+        }
+        
+
 }
 
 // FUNÇÃO PARA BUSCAR AS CIDADES DE CADA ESTADO
-function getCidades(sigla){
+function getCidades(sigla) {
         let resposta = {}
         let cidade = []
         let siglaUF = String(sigla).toUpperCase()
 
-        estadosUF.forEach(function(itemCidade){
-                if(siglaUF == String(itemCidade.sigla).toUpperCase()){
+        estadosUF.forEach(function (itemCidade) {
+
+                if (siglaUF == String(itemCidade.sigla).toUpperCase()) {
                         resposta = {
                                 uf: itemCidade.sigla,
                                 descricao: itemCidade.nome,
                                 quantidade: itemCidade.cidades.length
-                                
+
                         }
 
-                        itemCidade.cidades.forEach(function(cidadesEstado){
+                        itemCidade.cidades.forEach(function (cidadesEstado) {
                                 cidades = cidadesEstado.nome
                                 cidade.push(cidades)
-                                
+
                         })
                         resposta.cidades = cidade
                 }
 
 
         })
-
-        return resposta
+        if(resposta == null || siglaUF == '' || siglaUF == null || !isNaN(siglaUF) || siglaUF != resposta.uf){
+                return false
+        }else{
+                return resposta
+        }
+        
+        
 }
 
-//console.log(getCidades('ba'))
-//console.log(getCapitalEstado('Sp'))
+//console.log(getCidades('a'))
+//console.log(getCapitalEstado('sp'))
 //console.log(getDadosEstado('ba'))
 //console.log(getListaDeEstados())
-//console.log(getEstadosRegiao('sudeste'))
+console.log(getEstadosRegiao('sul'))
 //console.log(getCapitalPais())
+
+
